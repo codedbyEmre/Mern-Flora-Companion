f3bf8ee54f279d4c6ca93a12acbf74cb5039e971
codedbyemre <codedbyemre@gmail.com>
2023-08-23 12:02:59 +0300

    mongo db connection provided

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // imports
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const floraRoutes = require('./routes/floraRoutes');
 
 // create express app
@@ -9,7 +10,15 @@ const app = express();
 // routes
 app.use('/api/floras', floraRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('app listening on port', process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('app listening on port', process.env.PORT);
+    });
+  })
+  .catch(error => {
+    console.log('error :>> ', error);
+  });
