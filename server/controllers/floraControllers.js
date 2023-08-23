86f311c67afbc75db5b37173c57322044705d81f
codedbyemre <codedbyemre@gmail.com>
2023-08-23 13:20:53 +0300

    logics moved to controllers

---
+++ server/controllers/floraControllers.js
@@ -0,0 +1,88 @@
+// imports
+const Flora = require('../models/floraModels');
+const mongoose = require('mongoose');
+
+// get all floras
+const getFloras = async (req, res) => {
+  try {
+    const floras = await Flora.find({}).sort({ createdAt: -1 });
+    res.status(200).json(floras);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// get a single flora
+const getFlora = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    const flora = await Flora.findById(id);
+
+    if (!flora) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    res.status(200).json(flora);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// create a flora
+const createFlora = async (req, res) => {
+  try {
+    const flora = await Flora.create(req.body);
+    res.status(200).json(flora);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// update a flora
+const updateFlora = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    const flora = await Flora.findByIdAndUpdate({ _id: id }, { ...req.body });
+
+    if (!flora) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    res.status(200).json(flora);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a flora
+const deleteFlora = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    const flora = await Flora.findByIdAndDelete({ _id: id });
+
+    if (!flora) {
+      return res.status(404).json({ error: 'There is no such flora!' });
+    }
+
+    res.status(200).json(flora);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { getFloras, getFlora, createFlora, updateFlora, deleteFlora };
--- server/routes/floraRoutes.js
+++ server/routes/floraRoutes.js
@@ -1,91 +1,22 @@
 // imports
 const express = require('express');
-const mongoose = require('mongoose');
-const Flora = require('../models/floraModels');
+const { getFloras, getFlora, createFlora, updateFlora, deleteFlora } = require('../controllers/floraControllers');
 
 const router = express.Router();
 
 // GET all floras
-router.get('/', async (req, res) => {
-  try {
-    const floras = await Flora.find({}).sort({ createdAt: -1 });
-    res.status(200).json(floras);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get('/', getFloras);
 
 // GET a single flora
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    const flora = await Flora.findById(id);
-
-    if (!flora) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    res.status(200).json(flora);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get('/:id', getFlora);
 
 // POST a new flora
-router.post('/', async (req, res) => {
-  try {
-    const flora = await Flora.create(req.body);
-    res.status(200).json(flora);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/', createFlora);
 
 // UPDATE a flora
-router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    const flora = await Flora.findByIdAndUpdate({ _id: id }, { ...req.body });
-
-    if (!flora) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    res.status(200).json(flora);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.patch('/:id', updateFlora);
 
 // DELETE a flora
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    const flora = await Flora.findByIdAndDelete({ _id: id });
-
-    if (!flora) {
-      return res.status(404).json({ error: 'There is no such flora!' });
-    }
-
-    res.status(200).json(flora);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.delete('/:id', deleteFlora);
 
 module.exports = router;
