7606e7ffcb7c6c0a4b13f5d5ef73117c4b2b606f
codedbyemre <codedbyemre@gmail.com>
2023-08-25 10:46:09 +0300

    token created in server

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,10 @@
 // imports
 const User = require('../models/userModels');
+const jwt = require('jsonwebtoken');
+
+const createToken = id => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: '3d' });
+};
 
 // signup user
 const signup = async (req, res) => {
@@ -7,7 +12,10 @@ const signup = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -19,7 +27,10 @@ const login = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
-    res.status(200).json({ email, user });
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
